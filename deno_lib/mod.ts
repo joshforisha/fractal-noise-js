15813534c1f53beb9c62b3fd27b11711d0e400e0
Josh Forisha <josh@ario.com>
2021-04-19 10:53:01 -0500

    Setup Deno build

--- deno_lib/mod.ts
+++ deno_lib/mod.ts
@@ -1,3 +1,5 @@
+// This is free and unencumbered software released into the public domain
+
 const TWO_PI = 2 * Math.PI;
 
 export type Noise1Fn = (x: number) => number;
@@ -9,20 +11,22 @@ export type Noise3Fn = (x: number, y: number, z: number) => number;
 export type Noise4Fn = (x: number, y: number, z: number, w: number) => number;
 
 export interface Options {
-  amplitude?: number;
-  frequency?: number;
-  octaves?: number;
-  persistence?: number;
+  amplitude: number;
+  frequency: number;
+  octaves: number;
+  persistence: number;
 }
 
-function processOptions(options: Options): Options {
+function processOptions(options: Partial<Options>): Options {
   return {
     amplitude: typeof options.amplitude === "number" ? options.amplitude : 1.0,
     frequency: typeof options.frequency === "number" ? options.frequency : 1.0,
-    octaves:
-      typeof options.octaves === "number" ? Math.floor(options.octaves) : 1,
-    persistence:
-      typeof options.persistence === "number" ? options.persistence : 0.5
+    octaves: typeof options.octaves === "number"
+      ? Math.floor(options.octaves)
+      : 1,
+    persistence: typeof options.persistence === "number"
+      ? options.persistence
+      : 0.5,
   };
 }
 
@@ -31,10 +35,10 @@ export function makeCuboid(
   height: number,
   depth: number,
   noise3: Noise3Fn,
-  options: Options = {}
+  options: Partial<Options> = {},
 ): number[][] {
   const { amplitude, frequency, octaves, persistence } = processOptions(
-    options
+    options,
   );
   const field = new Array(width);
   for (let x = 0; x < width; x++) {
@@ -45,8 +49,7 @@ export function makeCuboid(
         let value = 0.0;
         for (let octave = 0; octave < octaves; octave++) {
           const freq = frequency * Math.pow(2, octave);
-          value +=
-            noise3(x * freq, y * freq, z * freq) *
+          value += noise3(x * freq, y * freq, z * freq) *
             (amplitude * Math.pow(persistence, octave));
         }
         field[x][y][z] = value / (2 - 1 / Math.pow(2, octaves - 1));
@@ -60,10 +63,10 @@ export function makeCylinderSurface(
   circumference: number,
   height: number,
   noise3: Noise3Fn,
-  options: Options = {}
+  options: Partial<Options> = {},
 ): number[] {
   const { amplitude, frequency, octaves, persistence } = processOptions(
-    options
+    options,
   );
   const radius = circumference / TWO_PI;
   const field = new Array(circumference);
@@ -76,8 +79,7 @@ export function makeCylinderSurface(
         const nx = x / circumference;
         const rdx = nx * TWO_PI;
         const [a, b] = [radius * Math.sin(rdx), radius * Math.cos(rdx)];
-        value +=
-          noise3(a * freq, b * freq, y * freq) *
+        value += noise3(a * freq, b * freq, y * freq) *
           (amplitude * Math.pow(persistence, octave));
       }
       field[x][y] = value / (2 - 1 / Math.pow(2, octaves - 1));
@@ -89,10 +91,10 @@ export function makeCylinderSurface(
 export function makeLine(
   length: number,
   noise1: Noise1Fn,
-  options: Options = {}
+  options: Partial<Options> = {},
 ): number[] {
   const { amplitude, frequency, octaves, persistence } = processOptions(
-    options
+    options,
   );
   const field = new Array(length);
   for (let x = 0; x < length; x++) {
@@ -110,10 +112,10 @@ export function makeRectangle(
   width: number,
   height: number,
   noise2: Noise2Fn,
-  options: Options = {}
+  options: Partial<Options> = {},
 ): number[] {
   const { amplitude, frequency, octaves, persistence } = processOptions(
-    options
+    options,
   );
   const field = new Array(width);
   for (let x = 0; x < width; x++) {
@@ -122,8 +124,7 @@ export function makeRectangle(
       let value = 0.0;
       for (let octave = 0; octave < octaves; octave++) {
         const freq = frequency * Math.pow(2, octave);
-        value +=
-          noise2(x * freq, y * freq) *
+        value += noise2(x * freq, y * freq) *
           (amplitude * Math.pow(persistence, octave));
       }
       field[x][y] = value / (2 - 1 / Math.pow(2, octaves - 1));
@@ -135,10 +136,10 @@ export function makeRectangle(
 export function makeSphereSurface(
   circumference: number,
   noise3: Noise3Fn,
-  options: Options = {}
+  options: Partial<Options> = {},
 ): number[] {
   const { amplitude, frequency, octaves, persistence } = processOptions(
-    options
+    options,
   );
   const field = new Array(circumference);
   for (let x = 0; x < circumference; x++) {
@@ -153,8 +154,7 @@ export function makeSphereSurface(
         const a = TWO_PI * Math.sin(rdx) * sinY;
         const b = TWO_PI * Math.cos(rdx) * sinY;
         const d = TWO_PI * Math.cos(rdy);
-        value +=
-          noise3(a * freq, b * freq, d * freq) *
+        value += noise3(a * freq, b * freq, d * freq) *
           (amplitude * Math.pow(persistence, octave));
       }
       field[x][y] = value / (2 - 1 / Math.pow(2, octaves - 1));
